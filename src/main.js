c3746e1357a8f9de5cdbbc8711d7d69f62111339
Farwa Syed <farwa.asyed@gmail.com>
2019-03-03 17:19:04 +1100

    basic

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+import Player from './Player.vue'
 
-Vue.config.productionTip = false
+import VueHowler from 'vue-howler'
+import router from './router'
+
+
+Vue.use(Vuetify)
+
+Vue.filter('numbers', (value) => {
+  let number = value + 1
+  if (number < 10) {
+    return "0" + number + "."
+  }
+  return number + "."
+})
+
+Vue.filter('minutes', (value) => {
+  if(!value || typeof value !== "number") return "00:00"
+  let min = parseInt(value / 60),
+      sec = parseInt(value % 60)
+  min = min < 10 ? "0" + min : min
+  sec = sec < 10 ? "0" + sec : sec
+  value = min + ":" + sec
+  return value
+})
+
+
+
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  router,
+  render: h => h(Player)
+})
